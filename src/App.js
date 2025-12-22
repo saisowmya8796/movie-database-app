a2c138187b61fe8a263ede8df566bb0d536beaaf
saisowmya8796 <saisowmya8796@gmail.com>
2025-12-22 14:52:56 +0000

    Set up application routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import {Switch, Route} from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import TopRated from './components/TopRated'
+import Upcoming from './components/Upcoming'
+import MovieDetails from './components/MovieDetails'
+import SearchResults from './components/SearchResults'
+
 import './App.css'
 
-// write your code here
-const App = () => <div>Hello World</div>
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/top-rated" component={TopRated} />
+        <Route path="/upcoming" component={Upcoming} />
+        <Route path="/movie/:id" component={MovieDetails} />
+        <Route path="/search" component={SearchResults} />
+      </Switch>
+    </>
+  )
+}
 
 export default App
